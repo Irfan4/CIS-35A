10489c5a85ead00454c44d46ba983b938fdf781a
Irfan Ahmad <31814510+Irfan4@users.noreply.github.com>
2019-07-21 03:18:23 -0700

    Update Simulation.java

--- lab2b/Simulation.java
+++ lab2b/Simulation.java
@@ -1,6 +1,5 @@
 
 /* Name - Irfan Ahmad
- * ID - 20364481
  * Course - CIS-35A
  * Description - This program will display the initial face up side of a coin, and then proceed to
  * toss it 20 times and produce the face up side for each iteration.  After all 20 tosses are complete,
